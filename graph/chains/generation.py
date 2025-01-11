d552ad150acec3fd05a68b28e769f0c2666e8524
man <mantianly@163.com>
2025-01-11 21:02:16 +0800

    Initial commit

---
+++ graph/chains/generation.py
@@ -0,0 +1,14 @@
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+import os
+llm = ChatOpenAI(
+    api_key=os.getenv("DASHSCOPE_API_KEY"),
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+    model="qwen-plus",
+    temperature=0,
+    # other params...
+)
+prompt = hub.pull("rlm/rag-prompt")
+
+generation_chain = prompt | llm | StrOutputParser()
